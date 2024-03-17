660f3263f006cdacacfeedb9f0a6d28e0a45fd66
Andreas Fiehn <andreas@lyhnefiehn.dk>
2024-03-17 12:45:23 +0100

    Get datasets from Stanford SNAP

--- graph_embeddings/data/make_datasets.py
+++ graph_embeddings/data/make_datasets.py
@@ -1,9 +1,56 @@
 # import pytorch geometric
 import torch
 import torch_geometric.datasets
+from torch_geometric.datasets import SNAPDataset
 from torch_geometric.utils import to_dense_adj
 
 from graph_embeddings.utils.config import Config
+import requests 
+import os
+import os.path as osp
+
+from torch_geometric.datasets.snap_dataset import read_wiki
+
+
+# Monekypatch: Add the ca-HepPh, ca-GrQc and p2p-Gnutella04 datasets to the available datasets in SNAPDataset
+SNAPDataset.available_datasets.update({
+    'ca-hepph': ['ca-HepPh.txt.gz'], # Add ca-HepPh dataset to available datasets.
+    'ca-grqc': ['ca-GrQc.txt.gz'], # Add ca-GrQc dataset to available datasets.
+    'p2p-gnutella04': ['p2p-Gnutella04.txt.gz'] # Add p2p-Gnutella04 dataset to available datasets.
+})
+print(SNAPDataset.available_datasets)
+
+# Save the original process method
+original_process = SNAPDataset.process
+
+# Make a new process method, monkey patching the original one
+def process_wrapper(self):
+    # The ca-HepPh dataset has the same format as the wiki-Vote dataset. Therefore, we can use the same processing function. 
+    # This is a temporary fix until the SNAPDataset class is updated to include the ca-HepPh dataset. Should probably do a pull request to the torch_geometric repo to include this dataset.
+    # ! Note: It seems like there is bug in the read_soc in snap_dataset.py. Looks like the shapes do not match with the graph data. Omit using it for now.
+    if self.name.startswith('ca-') or self.name.startswith('p2p-'):
+        raw_dir = osp.join(self.root, self.name, 'raw')
+        filenames = os.listdir(self.raw_dir)
+        if len(filenames) == 1 and osp.isdir(osp.join(raw_dir, filenames[0])):
+            raw_dir = osp.join(raw_dir, filenames[0])
+
+        raw_files = sorted([osp.join(raw_dir, f) for f in os.listdir(raw_dir)])
+        data_list = read_wiki(raw_files, self.name[5:])
+
+        if len(data_list) > 1 and self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        torch.save(self.collate(data_list), self.processed_paths[0])
+
+    else:
+        # Otherwise, use the original process method
+        original_process(self)
+
+# Apply the monkey patch
+SNAPDataset.process = process_wrapper
 
 
 def get_data_from_torch_geometric(paper, dataset_name, root='./data/raw'):
@@ -35,7 +82,6 @@ def get_adjacency_matrix(dataset):
     adj_matrix[adj_matrix > 0] = 1
     return adj_matrix
 
-
 def save_adjacency_matrix(adj_matrix, file_path):
     """
     Save the adjacency matrix to a file in PyTorch format (.pt)
@@ -43,7 +89,7 @@ def save_adjacency_matrix(adj_matrix, file_path):
     :param file_path: Path to the file where the adjacency matrix will be saved
     """
     torch.save(adj_matrix, file_path)
-    print(f"Adjacency matrix saved to {file_path}")
+    print(f"Adjacency matrix ({adj_matrix.shape}) saved to {file_path}")
 
 if __name__ == "__main__":
     cfg = Config("configs/config.yaml")
@@ -57,12 +103,24 @@ if __name__ == "__main__":
         dataset_name = key 
         src = val
 
+        print(f"Processing dataset: {dataset_name} from source: {src}")
+
         # if the sorce is pytorch-geometric 
-        if src and "pytorch-geometric" in src.lower():
+        if src and ("pytorch-geometric" in src.lower()):
             # split src string by /
             src_split = src.split("/")
             # get dataset
             dataset = get_data_from_torch_geometric(src_split[1], src_split[2], raw_path)
+            # get adjacency matrix
+            adj_matrix = get_adjacency_matrix(dataset)
+            # save adjacency matrix
+            new_file_path = f"{adj_matrix_path}/{dataset_name}.pt"
+            save_adjacency_matrix(adj_matrix, new_file_path)
+
+        elif src and "snapdataset" in src.lower():
+            src_split = src.split("/")
+            # get dataset
+            dataset = get_data_from_torch_geometric(src_split[0], src_split[1], raw_path)
             # get adjacency matrix
             adj_matrix = get_adjacency_matrix(dataset)
             # save adjacency matrix
