dcf019b3d9fe1f40cd6bd1109aa7aac5067cbb32
Andreas Fiehn <andreas@lyhnefiehn.dk>
2024-04-12 15:51:05 +0200

    Started working on CaseControl

--- graph_embeddings/utils/dataloader.py
+++ graph_embeddings/utils/dataloader.py
@@ -123,6 +123,74 @@ class CustomGraphDataLoader:
         A = A.fill_diagonal_(1)
         return A
 
+class CaseControl:
+    def __init__(self, data: torch_geometric.data.Data, batch_size: int, negative_sampling_ratio: int = 5):
+        self.data = data
+        self.batch_size = batch_size
+        self.num_nodes = data.num_nodes
+        self.num_parts = (self.num_nodes + self.batch_size - 1) // self.batch_size
+        self.neg_ratio = negative_sampling_ratio
+
+    def __iter__(self):
+        for sub_data in self.sampler:
+            yield Batch(sub_data)
+
+    def __len__(self):
+        return self.num_parts
+    
+
+    def __difference_set(self, a, b):
+        combined = torch.cat((a, b))
+        uniques, counts = combined.unique(return_counts=True)
+        difference = uniques[counts == 1]
+
+        return difference
+    
+    def __intersection_set(self, a, b):
+        combined = torch.cat((a, b))
+        uniques, counts = combined.unique(return_counts=True)
+        intersection = uniques[counts > 1]
+
+        return intersection
+
+    @cached_property
+    def all_possible_edges(self):
+        return torch.arange(self.num_nodes)
+
+    def sample_non_links(self, links):
+        num_links = len(links)
+        possible_non_links =  self.__difference_set(self.all_possible_edges, links)
+
+        non_links = possible_non_links[torch.randperm(possible_non_links.size(0))]
+        non_links = non_links[:self.neg_ratio*num_links]
+
+        return non_links
+
+    def sample(self, batch_size: int):
+        d = 5
+        # without replacemen
+        sources = torch.randperm(self.num_nodes)[:batch_size]
+        srcs_idxs = torch.cat([torch.where(self.data.edge_index[0] == src)[0] for src in sources])
+        trgts = self.data.edge_index[1][srcs_idxs]
+        srcs = self.data.edge_index[0][srcs_idxs]
+        link_edge_index = torch.stack([srcs, trgts], dim=0)
+
+        
+        all_non_links_target = []
+        all_non_links_source = []
+        for src in sources:
+            links = link_edge_index[1][link_edge_index[0] == src]
+            # difference between all possible edges and targets_
+            non_links = self.sample_non_links(links)
+
+            all_non_links_source.append(torch.ones_like(non_links)*src)
+            all_non_links_target.append(non_links)
+
+            intersection = self.__intersection_set(links, non_links) # TODO: remove this line
+            assert len(intersection) == 0, "Intersection between links and non-links is not empty"
+            
+        non_link_edge_index = torch.stack([torch.cat(all_non_links_source), torch.cat(all_non_links_target)], dim=0)
+     
 
 if __name__ == '__main__':
     from graph_embeddings.models.L2Model import L2Model
@@ -140,33 +208,37 @@ if __name__ == '__main__':
     data = dataset[0]
     adj_true = torch.load(f"{adj_matrices_path}/Cora.pt")
 
-    dataloader = CustomGraphDataLoader(data,batch_size=data.num_nodes)
-    model = L2Model.init_random(dataloader.num_total_nodes, dataloader.num_total_nodes, 50)
+
+    loader = CaseControl(data, 10)
+    print(loader.sample(10))
+
+    # dataloader = CustomGraphDataLoader(data,batch_size=data.num_nodes)
+    # model = L2Model.init_random(dataloader.num_total_nodes, dataloader.num_total_nodes, 50)
 
 
-    fulladj = dataloader.full_adj
-    compare = torch.all(fulladj == adj_true)
-    print("Adjacency matrices are equal:", compare.item())
+    # fulladj = dataloader.full_adj
+    # compare = torch.all(fulladj == adj_true)
+    # print("Adjacency matrices are equal:", compare.item())
 
-    data = dataset[0]
+    # data = dataset[0]
 
-    dataloader = CustomGraphDataLoader(data,batch_size=520)
-    model = L2Model.init_random(dataloader.num_total_nodes, dataloader.num_total_nodes, 50)
+    # dataloader = CustomGraphDataLoader(data,batch_size=520)
+    # model = L2Model.init_random(dataloader.num_total_nodes, dataloader.num_total_nodes, 50)
 
-    for b_idx, batch in enumerate(dataloader):
-        print("Batch:", b_idx)
-        idx = batch.indices
+    # for b_idx, batch in enumerate(dataloader):
+    #     print("Batch:", b_idx)
+    #     idx = batch.indices
 
-        # only take out a submatrix of the adj_true matrix
-        adj_true_sub = adj_true[idx][:,idx]
-        batch_adj = batch.adj
+    #     # only take out a submatrix of the adj_true matrix
+    #     adj_true_sub = adj_true[idx][:,idx]
+    #     batch_adj = batch.adj
 
-        # compare the adjacency matrices
-        compare = torch.all(adj_true_sub == batch_adj)
-        print("  Adjacency matrices in batch are equal:", compare.item())
+    #     # compare the adjacency matrices
+    #     compare = torch.all(adj_true_sub == batch_adj)
+    #     print("  Adjacency matrices in batch are equal:", compare.item())
 
-        print("  Batch adj shape:", batch.adj.shape)
-        print("  Batch adj_s shape:", batch.adj_s.shape)
-        print("  Batch indices shape:", batch.indices.shape)
-        print("  Reconstruct shape:", model.reconstruct(idx).shape)
-        break
+    #     print("  Batch adj shape:", batch.adj.shape)
+    #     print("  Batch adj_s shape:", batch.adj_s.shape)
+    #     print("  Batch indices shape:", batch.indices.shape)
+    #     print("  Reconstruct shape:", model.reconstruct(idx).shape)
+    #     break
